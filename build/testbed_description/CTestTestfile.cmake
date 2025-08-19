e690933cab8bff7586f4f8cf29c4c7ef851c796e
aravindsairam001 <aravindsairam98@gmail.com>
2025-08-19 12:40:29 +0530

    removed unwanted

--- build/testbed_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aero-ws/nav2_bot/testbed_description
-# Build directory: /home/aero-ws/nav2_bot/build/testbed_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/testbed_gazebo/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/aero-ws/nav2_bot/install/testbed_description/share/testbed_description/package.sh"
